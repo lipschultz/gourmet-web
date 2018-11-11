8692632d8ae5fc9f0337cce94991531da21e53a3
Michael Lipschultz <michael.lipschultz@gmail.com>
2018-11-11 15:22:07 -0500

    Use classes to indicate complete/incomplete

--- gourmetweb/static/scripts.js
+++ gourmetweb/static/scripts.js
@@ -2,9 +2,17 @@ function visualizeCheck(checkboxElem) {
   var elements = document.getElementsByName(checkboxElem.name);
   for (var i = 0; i < elements.length; i++ ) {
     if (checkboxElem.checked) {
-      elements[i].style.color = '#aaa';
+      if (elements[i].classList.contains('optional')) {
+        elements[i].classList.add('optional-complete');
+      } else {
+        elements[i].classList.add('complete');
+      }
     } else {
-      elements[i].style.color = '#000';
+      if (elements[i].classList.contains('optional-complete')) {
+        elements[i].classList.remove('optional-complete');
+      } else {
+        elements[i].classList.remove('complete');
+      }
     }
   }
 }
