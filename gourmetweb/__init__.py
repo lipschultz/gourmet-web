624a15893a241aaa66b1472b724934f01adea3cf
Michael Lipschultz <michael.lipschultz@gmail.com>
2020-11-14 17:08:54 -0500

    Clean up code

--- gourmetweb/__init__.py
+++ gourmetweb/__init__.py
@@ -3,20 +3,12 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE='/home/michael/projects/gourmet-web/gourmetweb/recipes.db',#os.path.join(app.instance_path, 'recipes.db'),
+        DATABASE=os.path.join(app.instance_path, 'recipes.db'),
     )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
